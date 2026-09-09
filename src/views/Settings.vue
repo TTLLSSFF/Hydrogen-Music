4033b16b38fd1e9025243a7b1131d48ec74edfb4
dongdong <ldx123000@gmail.com>
2026-09-09 15:29:40 +0800

    更新版本号至 0.6.3

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1018,7 +1018,7 @@ const toggleLocalOnlyMode = async () => {
                 <div class="app-icon">
                     <img src="../assets/icon/icon.ico" alt="" />
                 </div>
-                <div class="version">V0.6.2</div>
+                <div class="version">V0.6.3</div>
                 <div class="update-check">
                     <button class="check-update-btn" @click="checkForUpdates">检查更新</button>
                 </div>
